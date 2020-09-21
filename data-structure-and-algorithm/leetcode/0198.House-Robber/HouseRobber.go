9b2d9717f3009bd4b71beee0ba4bbcc32392b61a
changqingyeh <yecq1998@outlook.com>
2020-09-21 22:55:24 +0800

    :sparkles: 0198.House-Robber

---
+++ data-structure-and-algorithm/leetcode/0198.House-Robber/HouseRobber.go
@@ -0,0 +1,26 @@
+package leetcode
+
+func rob(nums []int) int {
+	if len(nums) == 0 {
+		return 0
+	}
+	if len(nums) == 1 {
+		return nums[0]
+	}
+	if len(nums) == 2 {
+		if nums[0] > nums[1] {
+			nums[1] = nums[0]
+			return nums[0]
+		}
+		return nums[1]
+	}
+	for i := 2; i < len(nums); i++ {
+		sum := nums[i] + nums[i-2]
+		if sum > nums[i-1] {
+			nums[i] = sum
+		} else {
+			nums[i] = nums[i-1]
+		}
+	}
+	return nums[len(nums)-1]
+}
---
+++ data-structure-and-algorithm/leetcode/0198.House-Robber/HouseRobber_test.go
@@ -0,0 +1,23 @@
+package leetcode
+
+import "testing"
+
+func Test_rob(t *testing.T) {
+	type args struct {
+		nums []int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{"case", args{[]int{1, 2, 3, 1}}, 4},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := rob(tt.args.nums); got != tt.want {
+				t.Errorf("rob() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
