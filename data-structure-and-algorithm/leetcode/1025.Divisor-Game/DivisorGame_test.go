36da6434452b47bf964aad2854f94ebc5e895cd4
changqingyeh <yecq1998@outlook.com>
2020-09-23 22:53:12 +0800

    :sparkles: 1025.Divisor-Game

---
+++ data-structure-and-algorithm/leetcode/1025.Divisor-Game/DivisorGame_test.go
@@ -0,0 +1,24 @@
+package leetcode
+
+import "testing"
+
+func Test_divisorGame(t *testing.T) {
+	type args struct {
+		N int
+	}
+	tests := []struct {
+		name string
+		args args
+		want bool
+	}{
+		{"case1", args{2}, true},
+		{"case2", args{3}, false},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := divisorGame(tt.args.N); got != tt.want {
+				t.Errorf("divisorGame() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
