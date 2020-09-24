de6a4f373fd6cd1fe02161d8e4462231c1224b77
changqingyeh <yecq1998@outlook.com>
2020-09-24 22:41:19 +0800

    Add tree demo

---
+++ data-structure-and-algorithm/basic/tree/Tree.go
@@ -0,0 +1,55 @@
+package tree
+
+// TreeNode 树节点
+type TreeNode struct {
+	val   int
+	left  *TreeNode
+	right *TreeNode
+}
+
+func (tree *TreeNode) preOrderTraverse() []int {
+	res := []int{}
+	if tree == nil {
+		return res
+	}
+	stack := []*TreeNode{}
+	for tree != nil || len(stack) != 0 {
+		if tree != nil {
+			res = append(res, tree.val)
+			stack = append(stack, tree)
+			tree = tree.left
+		} else {
+			tree = stack[len(stack)-1]
+			stack = stack[:len(stack)-1]
+			tree = tree.right
+		}
+	}
+	return res
+}
+
+func (tree *TreeNode) inOrderTraverse() []int {
+	res := []int{}
+	if tree == nil {
+		return res
+	}
+	stack := []*TreeNode{}
+	for tree != nil || len(stack) != 0 {
+		if tree != nil {
+			stack = append(stack, tree)
+			tree = tree.left
+		} else {
+			tree = stack[len(stack)-1]
+			res = append(res, tree.val)
+			stack = stack[:len(stack)-1]
+			tree = tree.right
+		}
+	}
+	return res
+}
+
+func (tree *TreeNode) postOrderTraverse() []int {
+	res := []int{}
+	if tree != nil {
+		return res
+	}
+}
