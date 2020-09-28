e0b81de41ff1f90e5e29386a25e46311346788ff
changqingyeh <yecq1998@outlook.com>
2020-09-28 22:33:28 +0800

    :sparkles: leetcode/0062.Unique-Paths

---
+++ data-structure-and-algorithm/leetcode/0062.Unique-Paths/UniquePaths_test.go
@@ -0,0 +1,25 @@
+package leetcode
+
+import "testing"
+
+func Test_uniquePaths(t *testing.T) {
+	type args struct {
+		m int
+		n int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{"case1", args{3, 2}, 3},
+		{"case1", args{3, 7}, 28},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := uniquePaths(tt.args.m, tt.args.n); got != tt.want {
+				t.Errorf("uniquePaths() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
