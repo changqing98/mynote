cd10fa69053e1bbb41c73598e2258f9205295524
changqingyeh <yecq1998@outlook.com>
2020-10-11 23:06:19 +0800

    测试C语言extern关键字

---
+++ c&c++/c&c++-demo/basic/extern_test1.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+int a = 10;
+
+void do_print()
+{
+  printf(a);
+}
---
+++ c&c++/c&c++-demo/basic/extern_test_main.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include "extern_test1.c"
+extern int a;
+
+int main(){
+  printf("%d", a);
+}
