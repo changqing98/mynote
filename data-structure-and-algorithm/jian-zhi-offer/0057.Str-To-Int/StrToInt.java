72c1bdc356eb4e50e9e54f2f6e20a006a11372cd
changqingyeh <yecq1998@outlook.com>
2020-10-12 22:25:26 +0800

    0057.Str-To-Int/StrToInt

--- data-structure-and-algorithm/jian-zhi-offer/0057.Str-To-Int/StrToInt.java
+++ data-structure-and-algorithm/jian-zhi-offer/0057.Str-To-Int/StrToInt.java
@@ -1,6 +1,4 @@
-import java.sql.Blob;
-
-public class StrToInt{
+public class StrToInt {
 
     public static int strToInt(String str) {
         int factor = 1;
@@ -10,28 +8,28 @@ public class StrToInt{
         int cur;
         int threadhold = Integer.MAX_VALUE / 10;
         int i = 0;
-        while(i < str.length() && str.charAt(i) == ' '){
+        while (i < str.length() && str.charAt(i) == ' ') {
             i++;
         }
-        if(i >= str.length()){
+        if (i >= str.length()) {
             return 0;
         }
-        if(array[i] == '-'){
+        if (array[i] == '-') {
             factor = -1;
             i++;
-        } else if (array[i] == '+'){
+        } else if (array[i] == '+') {
             i++;
         }
-        for(; i < str.length(); i++){
+        for (; i < str.length(); i++) {
             c = str.charAt(i);
-            if(c < '0' || c > '9'){
+            if (c < '0' || c > '9') {
                 return result * factor;
             }
             cur = c - '0';
-            if (factor > 0 && (result == threadhold && cur >= 7 || result > threadhold)){
+            if (factor > 0 && (result == threadhold && cur >= 7 || result > threadhold)) {
                 return Integer.MAX_VALUE;
             }
-            if(factor < 0 && (result == threadhold && cur >= 8 || result > threadhold)){
+            if (factor < 0 && (result == threadhold && cur >= 8 || result > threadhold)) {
                 return Integer.MIN_VALUE;
             }
             result = result * 10 + cur;
