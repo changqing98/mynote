4d37341a9063f09de971eb03e4023d8e3671227f
changqingyeh <yecq1998@outlook.com>
2020-11-02 23:18:52 +0800

    leetcode/0024.SwapPairs

--- data-structure-and-algorithm/leetcode/0024.SwapPairs/SwapPairs.go
+++ data-structure-and-algorithm/leetcode/0024.SwapPairs/SwapPairs.go
@@ -1,1 +1,16 @@
-package _024_SwapPairs
+package leetcode
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func swapPairs(head *ListNode) *ListNode {
+	if head == nil || head.Next == nil {
+		return head
+	}
+	var p = head.Next
+	head.Next = swapPairs(head.Next.Next)
+	p.Next = head
+	return p
+}
