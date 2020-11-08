d94edc1e2e88ca9b2276285714bf19a631e282c2
changqingyeh <yecq1998@outlook.com>
2020-11-08 23:13:25 +0800

    0039.CombinationSum

--- data-structure-and-algorithm/leetcode/0039.CombinationSum/CombinationSum.go
+++ data-structure-and-algorithm/leetcode/0039.CombinationSum/CombinationSum.go
@@ -2,7 +2,7 @@ package leetcode
 
 func combinationSum(candidates []int, target int) [][]int {
 	if len(candidates) == 1 {
-		if target % candidates[0] == 0 {
+		if target%candidates[0] == 0 {
 			var count = target / candidates[0]
 			var res = make([][]int, 1)
 			res[0] = make([]int, count)
@@ -16,23 +16,25 @@ func combinationSum(candidates []int, target int) [][]int {
 	}
 	var res [][]int
 	var tmp []int
-	helper(candidates, target, tmp, &res, 0)
+	helper(candidates, target, tmp, &res, 0, 0)
 	return res
 }
 
-func helper(candidates []int, target int, tmp []int, result *[][]int, k int) {
+func helper(candidates []int, target int, tmp []int, result *[][]int, sum int, k int) {
+	if sum == target {
+		var item = make([]int, len(tmp))
+		copy(item, tmp)
+		*result = append(*result, item)
+		return
+	}
+	if sum > target {
+		return
+	}
 	for i := k; i < len(candidates); i++ {
-		if candidates[i] == target {
-			tmp = append(tmp, candidates[i])
-			var item = make([]int, len(tmp))
-			copy(item, tmp)
-			*result = append(*result, item)
-			return
-		} else if candidates[i] < target {
-			tmp = append(tmp, candidates[i])
-			helper(candidates, target-candidates[i], tmp, result, i)
-		} else {
-			return
-		}
+		sum += candidates[i]
+		tmp = append(tmp, candidates[i])
+		helper(candidates, target, tmp, result, sum, i)
+		sum -= candidates[i]
+		tmp = tmp[:len(tmp)-1]
 	}
 }
