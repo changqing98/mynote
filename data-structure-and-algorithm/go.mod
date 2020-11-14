b3529ffd61fcd866fdd4e7ac9ca43dcca83f22ce
changqingyeh <yecq1998@outlook.com>
2020-11-14 21:03:15 +0800

    0006.zigzag-conversion/ZigZagConversion

--- data-structure-and-algorithm/go.mod
+++ data-structure-and-algorithm/go.mod
@@ -1,3 +1,12 @@
 module data-structure-and-algorithm
 
 go 1.13
+
+require (
+	github.com/ghodss/yaml v1.0.0 // indirect
+	github.com/go-openapi/spec v0.19.12 // indirect
+	k8s.io/api v0.19.4 // indirect
+	k8s.io/client-go v11.0.0+incompatible // indirect
+	k8s.io/kube-openapi v0.0.0-20201113171705-d219536bb9fd // indirect
+	sigs.k8s.io/kustomize v2.0.3+incompatible
+)
---
+++ data-structure-and-algorithm/leetcode/0006.zigzag-conversion/ZigZagConversion.go
@@ -0,0 +1,43 @@
+package leetcode
+
+func convert(s string, numRows int) string {
+	length := len(s)
+	if length <= numRows || numRows == 1 {
+		return s
+	}
+	result := make([]byte, 0)
+	groupLength := 2*numRows - 2
+	groups := length / groupLength
+	if length%groupLength != 0 {
+		groups++
+	}
+	for i := 0; i < numRows; i++ {
+		if i == 0 || i == numRows-1 {
+			for j := 0; j < groups; j++ {
+				target := j*groupLength + i
+				if target < length {
+					result = append(result, s[target])
+				} else {
+					break
+				}
+			}
+		}
+		if i > 0 && i < numRows-1 {
+			for j := 0; j < groups; j++ {
+				target := j*groupLength + i
+				if target < length {
+					result = append(result, s[target])
+				} else {
+					break
+				}
+				target = j*groupLength + (groupLength - i)
+				if target < length {
+					result = append(result, s[target])
+				} else {
+					break
+				}
+			}
+		}
+	}
+	return string(result)
+}
