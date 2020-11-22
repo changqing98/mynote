467e072987ea7b9455dcc32288b336a0aa278319
changqingyeh <yecq1998@outlook.com>
2020-11-22 20:29:21 +0800

    221. Maximal Square

---
+++ data-structure-and-algorithm/leetcode/0221.MaximalSquare/MaximalSquare.java
@@ -0,0 +1,17 @@
+public class MaximalSquare{
+    public int maximalSquare(char[][] matrix) {
+        int rows = matrix.length;
+        int cols = rows > 0 ? matrix[0].length : 0;
+        int maxLength = 0;
+        int[][] dp = new int[rows + 1][cols + 1]; 
+        for(int i = 1; i<= rows; i++){
+            for(int j = 1; j <= cols; j++){
+                if(matrix[i-1][j-1] == '1'){
+                    dp[i][j] = Math.min(dp[i-1][j], Math.min(dp[i-1][j-1], dp[i][j-1])) + 1;
+                    maxLength = Math.max(maxLength, dp[i][j]);
+                }
+            }
+        }
+        return maxLength * maxLength;
+    }
+}
