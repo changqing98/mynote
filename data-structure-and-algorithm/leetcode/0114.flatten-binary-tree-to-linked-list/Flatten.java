14c51ff7984bf106ce4809b6297e137f8c6a0ddf
changqingyeh <yecq1998@outlook.com>
2020-12-01 23:31:28 +0800

    0114.flatten-binary-tree-to-linked-list

---
+++ data-structure-and-algorithm/leetcode/0114.flatten-binary-tree-to-linked-list/Flatten.java
@@ -0,0 +1,13 @@
+public class Flatten {
+    TreeNode prev;
+    public void flatten(TreeNode root) {
+        if(root == null){
+            return;
+        }
+        flatten(root.right);
+        flatten(root.left);
+        root.left = null;
+        root.right = prev;
+        prev = root;
+    }
+}
