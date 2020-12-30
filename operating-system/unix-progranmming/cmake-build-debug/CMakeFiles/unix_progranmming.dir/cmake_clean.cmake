a215fe54a235a85a836cc909f249615bdf6fd003
changqingyeh <yecq1998@outlook.com>
2020-12-30 13:06:22 +0800

    Refactor unix programming

---
+++ operating-system/unix-progranmming/cmake-build-debug/CMakeFiles/unix_progranmming.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/unix_progranmming.dir/fs/file.c.o"
+  "unix_progranmming"
+  "unix_progranmming.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/unix_progranmming.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
