93f35dfb8a4c5d533020ac4775986b5d75ba08ff
changqingyeh <yecq1998@outlook.com>
2021-03-02 22:53:27 +0800

    :Refactor datastrcuture

---
+++ data-structure-and-algorithm/leetcode/0024.SwapPairs/SwapPairs.java
@@ -0,0 +1,11 @@
+public class SwapPairs {
+	public ListNode swapPairs(ListNode head) {
+		if(head == null || head.next == null){
+			return head;
+		}
+		ListNode next = head.next;
+		head.next = swapPairs(next.next);
+		next.next = head;
+		return next;
+	}
+}
